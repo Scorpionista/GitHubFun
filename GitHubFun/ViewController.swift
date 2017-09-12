91f2839e569cf7a89a2320ca18f29298e35e4c93
Michael Donnelly <scorpionistamac@MacBook-Pro.local>
2017-09-11 22:32:23 -0600

    Initial Commit

---
+++ GitHubFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubFun
+//
+//  Created by Michael Donnelly on 9/11/17.
+//  Copyright © 2017 Michael Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
